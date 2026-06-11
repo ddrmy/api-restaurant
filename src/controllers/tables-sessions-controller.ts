26f384ea14a1f5e5afe4cf5007357509880ac9a8
Gabriel Sarti Fernandes Roberti <gabriel.roberti@onplug.com.br>
2026-06-11 16:51:51 -0300

    add: refining the table-sessions logic to check and prevent the creation of a session within an already existing and open session.

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,9 +1,32 @@
+import { db } from '@/database/knex'
+import { AppError } from '@/utils/AppErros'
 import { Request, Response, NextFunction } from 'express'
+import { z } from 'zod'
 
 export class TablesSessionsController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      return res.status(201).json({ message: 'Ok!' })
+      const bodySchema = z.object({
+        table_id: z.number(),
+      })
+
+      const { table_id } = bodySchema.parse(req.body)
+
+      const session = await db<TablesSessionsRepository>('tables_sessions')
+        .where({ table_id })
+        .orderBy('opened_at', 'desc')
+        .first()
+
+      if (session && !session.closed_at) {
+        throw new AppError('this table is already open')
+      }
+
+      await db<TablesSessionsRepository>('tables_sessions').insert({
+        table_id,
+        opened_at: db.fn.now(),
+      })
+
+      return res.status(201).json()
     } catch (error) {
       next(error)
     }
